34670ecb07a8bec14a0b43609d4d845d058ae2f3
Utkarsh <105288469+Curious-Utkarsh@users.noreply.github.com>
2022-07-01 16:49:34 +0530

    Update parsing_fullBody_Landmarks_using_Class.py

--- parsing_fullBody_Landmarks_using_Class.py
+++ parsing_fullBody_Landmarks_using_Class.py
@@ -12,15 +12,15 @@ cam.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*'MJPG'))
 
 class mpBody:
     import mediapipe as mp
-    def __init__(self,tol1=.5,tol2=.5):
-        self.bodyDetect=self.mp.solutions.pose.Pose(False,False,tol1,tol2)
+    def __init__(self,stillImage=False,upperBody=False,smoothData=True,tol1=.5,tol2=.5):
+        self.bodyDetect=self.mp.solutions.pose.Pose(stillImage,upperBody,smoothData,tol1,tol2)
         self.mpDraw=self.mp.solutions.drawing_utils
     def parseLandMarks(self,frame):
         frameRGB=cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
         results=self.bodyDetect.process(frameRGB)
+        myBody=[]
         if results.pose_landmarks != None:
             self.mpDraw.draw_landmarks(frame,results.pose_landmarks,self.mp.solutions.pose.POSE_CONNECTIONS)
-            myBody=[]
             for LandMark in results.pose_landmarks.landmark:
                 myBody.append((int(LandMark.x*width),int(LandMark.y*height)))
         return myBody
@@ -30,8 +30,9 @@ findBody=mpBody()
 while True:
     ignore,frame=cam.read()
     myBody=findBody.parseLandMarks(frame)
-    for i in [0,2,5]:
-        cv2.circle(frame,myBody[i],20,(0,255,255),2)    
+    if len(myBody)!=0:#prevents program from crashing when it doesnot detect any body or hand or any other landmarks
+        for i in [0,2,5]:
+            cv2.circle(frame,myBody[i],20,(0,255,255),2)    
     cv2.imshow('my Cam',frame)
     cv2.moveWindow('my Cam',0,0)
     if cv2.waitKey(1) & 0xff==ord('q'):
